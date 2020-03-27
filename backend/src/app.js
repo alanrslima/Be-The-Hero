a81ca7e008eedb6d83213495c7e8983dba073621
alanrslima <alanronison.lima@gmail.com>
2020-03-27 17:27:20 -0300

    Adicionado testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-
-app.listen(3333, () => {
-  console.log('Server running at 3333 port');
-});
+module.exports = app;
